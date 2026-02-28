ad1d2ba4231ec32df043e0ca23d1ce2156ab6af0
Jod <jod@sparkon.ai>
2026-02-28 16:24:22 +0800

    feat: move main.py to root folder for gcloud deployment

--- main.py
+++ main.py
@@ -53,3 +53,8 @@ def root():
         "message": "Welcome to Kita API", 
         "docs_url": "/docs"
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
